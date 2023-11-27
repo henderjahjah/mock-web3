6bd4a34c59fc3ed3bdf0daea28e50abec460feb2
henderjahjah <116949141+henderjahjah@users.noreply.github.com>
2023-11-27 13:13:51 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -21,3 +21,5 @@ const config = {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
