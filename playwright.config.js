064ecb1634c0a7daaca2a2625936a89ebdf1afac
henderjahjah <116949141+henderjahjah@users.noreply.github.com>
2023-11-29 11:15:11 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -25,3 +25,5 @@ const config = {
     timeout: 5000
   },
   /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
