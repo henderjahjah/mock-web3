8d13826fa33f5713eec8017fa4ba4f4573e171b5
henderjahjah <116949141+henderjahjah@users.noreply.github.com>
2023-12-02 11:36:54 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -31,3 +31,5 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
