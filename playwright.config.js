c0dfa1c0cad5250e2f3eabced7b249bcd5352765
henderjahjah <116949141+henderjahjah@users.noreply.github.com>
2023-12-08 10:59:35 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -38,3 +38,4 @@ const config = {
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
